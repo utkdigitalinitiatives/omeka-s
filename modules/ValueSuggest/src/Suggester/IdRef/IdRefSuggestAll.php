f87c0ffef020e442feb08fec2ffcbb64a2d249a6
Mat Jordan <mat@utk.edu>
2020-10-15 13:23:18 -0400

    Add ValueSuggest 1.5.0

---
+++ modules/ValueSuggest/src/Suggester/IdRef/IdRefSuggestAll.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace ValueSuggest\Suggester\IdRef;
+
+use ValueSuggest\Suggester\SuggesterInterface;
+use Zend\Http\Client;
+
+class IdRefSuggestAll implements SuggesterInterface
+{
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * @var string
+     */
+    protected $url;
+
+    public function __construct(Client $client, $url)
+    {
+        $this->client = $client;
+        $this->url = $url;
+    }
+
+    /**
+     * Retrieve suggestions from the IdRef web services API (based on Solr).
+     *
+     * @see https://www.idref.fr
+     * @param string $query
+     * @param string $lang
+     * @return array
+     */
+    public function getSuggestions($query, $lang = null)
+    {
+        // Convert the query into a Solr query.
+        $query = trim($query);
+        if (strpos($query, ' ')) {
+            $query = '(' . implode('%20AND%20', array_map('urlencode', explode(' ', $query))) . ')';
+        } else {
+            $query = urlencode($query);
+        }
+        $url = $this->url . $query;
+
+        $response = $this->client->setUri($url)->send();
+        if (!$response->isSuccess()) {
+            return [];
+        }
+
+        // Parse the JSON response.
+        $suggestions = [];
+        $results = json_decode($response->getBody(), true);
+
+        if (empty($results['response']['docs'])) {
+            return [];
+        }
+
+        // Check the result key.
+        if (isset($results['response']['docs'][0]['affcourt_r'])) {
+            $keyValue = 'affcourt_r';
+        } elseif (isset($results['response']['docs'][0]['affcourt_z'])) {
+            $keyValue = 'affcourt_z';
+        } elseif (isset($results['response']['docs'][0]['ppn_z'])) {
+            $keyValue = 'ppn_z';
+        } else {
+            return [];
+        }
+        foreach ($results['response']['docs'] as $result) {
+            $suggestions[] = [
+                'value' => is_array($result[$keyValue]) ? $result[$keyValue][0] : $result[$keyValue],
+                'data' => [
+                    'uri' => 'https://www.idref.fr/' . $result['ppn_z'],
+                    'info' => null,
+                ],
+            ];
+        }
+
+        return $suggestions;
+    }
+}
