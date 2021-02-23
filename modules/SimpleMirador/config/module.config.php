aa047b0885ea4a81f34492a3ec706b56d0c4800a
Mat Jordan <mat@utk.edu>
2021-02-23 15:17:11 -0500

    Add block-layout from admin side, call frontend view render.

--- modules/SimpleMirador/config/module.config.php
+++ modules/SimpleMirador/config/module.config.php
@@ -1,5 +1,18 @@
 <?php
 
-return [];
+namespace SimpleMirador;
+
+return [
+    'view_manager' => [
+        'template_path_stack' => [
+            dirname(__DIR__) . '/view',
+        ],
+    ],
+    'block_layouts' => [
+        'invokables' => [
+            'simplemirador' => Site\BlockLayout\SimpleMirador::class,
+        ],
+    ],
+];
 
 ?>
