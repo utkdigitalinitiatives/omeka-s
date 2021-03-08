1d40b7ae32a649572c4a427afb42c130cb71e0c4
Mat Jordan <mat@utk.edu>
2021-03-07 23:47:22 -0500

    Convert build method from gulp to webpack.

---
+++ themes/utk-exhibits/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    entry: ['./asset/sass/style.scss'],
+    output: {
+        path: path.resolve(__dirname, 'asset'),
+        filename: 'js/build.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'css/[name].css',
+                        }
+                    },
+                    {
+                        loader: 'extract-loader'
+                    },
+                    {
+                        loader: 'css-loader?-url'
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    }
+};
